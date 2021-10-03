982b0a2ed9a59d522cd579ddf0a6274d39da1c15
King_Hornet <robertzacchigna@gmail.com>
2021-10-03 03:14:56 -0500

    Revert and fix JS change for nav bar highlighting

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,21 @@ $(window).bind('load resize', function(event) {
   document.styleSheets[2].cssRules[0].style.setProperty("--nav-height", document.getElementById("nav-head").offsetHeight + 'px');
 });
 
+<!-- Nav Bar Page Highlighting -->
+$(function(){
+    $('a').each(function(){
+        var win_href = window.location.href;
+
+        if ($(this).prop('href') == win_href) {
+            $(this).parent('div').addClass('active');
+        } else if ($(this).prop('href') == win_href.slice(0, -1)) {
+            $(this).parent('div').addClass('active');
+        } else {
+            $(this).parent('div').removeClass('active');
+        }
+    });
+});
+
 <!-- Render Notebook Frames on Button Click -->
 document.addEventListener('DOMContentLoaded', () => {
     var myBtns = document.getElementsByTagName("input")
